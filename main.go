4f61a199799e87a730099f19529849ac193d80c9
RemBog <remedialbog88@gmail.com>
2025-06-13 16:16:01 +0200

    Remove timezone (not necessary)

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"EndioriteAPI/config"
 	"EndioriteAPI/database"
-	"os"
 )
 
 func main() {
-	os.Setenv("TZ", "Europe/Paris")
-
 	config.LoadEnv()
 	database.ConnectMySQL()
 	StartServer()
